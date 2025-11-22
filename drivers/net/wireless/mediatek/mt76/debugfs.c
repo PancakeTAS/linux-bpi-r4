81e75b2bd9fc99277402f85f6c6d1a45654eec4e
PancakeTAS <pancake@mgnet.work>
2025-11-22 09:13:44 +0100

    patch(mt76): update mt76 driver
    This updates the following files from MT76 to f473d66ab595d5f37aedc868bfe407da0a1245c8
    
    mt7996/(everything)
    mmio.c util.c trace.c dma.c mac80211.c debugfs.c eeprom.c tx.c agg-rx.c mcu.c wed.c scan.c channel.c
    mt76_connac_mcu.c mt76_connac_mac.c mt76_connac3_mac.c
    testmode.c
    pci.c

--- drivers/net/wireless/mediatek/mt76/debugfs.c
+++ drivers/net/wireless/mediatek/mt76/debugfs.c
@@ -93,9 +93,9 @@ void mt76_seq_puts_array(struct seq_file *file, const char *str,
 {
 	int i;
 
-	seq_printf(file, "%10s:", str);
+	seq_printf(file, "%16s:", str);
 	for (i = 0; i < len; i++)
-		seq_printf(file, " %2d", val[i]);
+		seq_printf(file, " %4d", val[i]);
 	seq_puts(file, "\n");
 }
 EXPORT_SYMBOL_GPL(mt76_seq_puts_array);
@@ -109,8 +109,8 @@ mt76_register_debugfs_fops(struct mt76_phy *phy,
 	struct dentry *dir;
 
 	dir = debugfs_create_dir("mt76", phy->hw->wiphy->debugfsdir);
-
 	debugfs_create_u8("led_pin", 0600, dir, &phy->leds.pin);
+	debugfs_create_bool("led_active_low", 0600, dir, &phy->leds.al);
 	debugfs_create_u32("regidx", 0600, dir, &dev->debugfs_reg);
 	debugfs_create_file_unsafe("regval", 0600, dir, dev, fops);
 	debugfs_create_file_unsafe("napi_threaded", 0600, dir, dev,
--- drivers/net/wireless/mediatek/mt76/mac80211.c
+++ drivers/net/wireless/mediatek/mt76/mac80211.c
@@ -613,23 +613,20 @@ EXPORT_SYMBOL_GPL(mt76_unregister_phy);
 
 int mt76_create_page_pool(struct mt76_dev *dev, struct mt76_queue *q)
 {
-	bool is_qrx = mt76_queue_is_rx(dev, q);
 	struct page_pool_params pp_params = {
 		.order = 0,
 		.flags = 0,
 		.nid = NUMA_NO_NODE,
 		.dev = dev->dma_dev,
 	};
-	int idx = is_qrx ? q - dev->q_rx : -1;
-
-	/* Allocate page_pools just for rx/wed_tx_free queues */
-	if (!is_qrx && !mt76_queue_is_wed_tx_free(q))
-		return 0;
+	int idx = q - dev->q_rx;
 
 	switch (idx) {
 	case MT_RXQ_MAIN:
 	case MT_RXQ_BAND1:
 	case MT_RXQ_BAND2:
+	case MT_RXQ_NPU0:
+	case MT_RXQ_NPU1:
 		pp_params.pool_size = 256;
 		break;
 	default:
@@ -643,9 +640,6 @@ int mt76_create_page_pool(struct mt76_dev *dev, struct mt76_queue *q)
 		pp_params.dma_dir = DMA_FROM_DEVICE;
 		pp_params.max_len = PAGE_SIZE;
 		pp_params.offset = 0;
-		/* NAPI is available just for rx queues */
-		if (idx >= 0 && idx < ARRAY_SIZE(dev->napi))
-			pp_params.napi = &dev->napi[idx];
 	}
 
 	q->page_pool = page_pool_create(&pp_params);
@@ -814,6 +808,7 @@ void mt76_free_device(struct mt76_dev *dev)
 		destroy_workqueue(dev->wq);
 		dev->wq = NULL;
 	}
+	mt76_npu_deinit(dev);
 	ieee80211_free_hw(dev->hw);
 }
 EXPORT_SYMBOL_GPL(mt76_free_device);
@@ -1553,7 +1548,8 @@ void mt76_rx_poll_complete(struct mt76_dev *dev, enum mt76_rxq_id q,
 
 	while ((skb = __skb_dequeue(&dev->rx_skb[q])) != NULL) {
 		mt76_check_sta(dev, skb);
-		if (mtk_wed_device_active(&dev->mmio.wed))
+		if (mtk_wed_device_active(&dev->mmio.wed) ||
+		    mt76_npu_device_active(dev))
 			__skb_queue_tail(&frames, skb);
 		else
 			mt76_rx_aggr_reorder(skb, &frames);
